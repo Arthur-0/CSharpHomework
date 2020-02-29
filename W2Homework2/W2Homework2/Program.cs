9b2a1e7dbe0776f651ae4c387f44012cbd4acab5
Arthur-0 <1015982558@qq.com>
2020-02-29 11:34:10 +0800

    homework

---
+++ W2Homework2/W2Homework2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace W2Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 9, 2, 6, 3, 8 };
+            Console.WriteLine("数组为：{ 1, 9, 2, 6, 3, 8 }" );
+            double average;
+            Console.WriteLine("数组的最大值为："+Max(a));
+            Console.WriteLine("数组的最小值为：" + Min(a));
+            Console.WriteLine("数组的所有元素的和为："+Sum(a, out average));
+            Console.WriteLine("数组的所有元素的平均值为：" + average);
+        }
+        static int Max(int[] array)
+        {
+            int Max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (Max < array[i])
+                    Max = array[i];
+            }
+            return Max;
+        }
+
+        static int Min(int[] array)
+        {
+            int Min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (Min > array[i])
+                    Min = array[i];
+            }
+            return Min;
+        }
+
+        static int Sum(int[] array, out double average)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                sum += array[i];
+            average = (double)sum / (double)array.Length;
+            return sum;
+        }
+    }
+}
---
+++ W2Homework3/W2Homework3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace W2Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prime();
+        }
+        static void prime()
+        {
+            int[] a = new int[101];
+            for (int i = 0; i < 101; i++)
+            {
+                a[i] = i;
+            }
+            a[1] = 0;
+            for (int i = 2; i * i < 100; i++)
+            {
+                for (int j = 2; j < 100; j++)
+                {
+                    while (a[j] != i && a[j] % i == 0 && a[j] != 0)
+                    {
+                        a[j] = 0;
+                    }
+                }
+            }
+            for (int i = 2; i < 100; i++)
+            {
+                if (a[i] != 0)
+                {
+                    Console.Write(a[i]);
+                    Console.Write(" ");
+                }  
+            }
+        }
+    }
+}
