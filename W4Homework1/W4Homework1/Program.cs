f7b5674e7f69366b95a42729fa14bbf984485f2e
Arthur-0 <1015982558@qq.com>
2020-03-14 12:10:11 +0800

    homework

---
+++ W4Homework1/W4Homework1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace W4Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> genericList = new GenericList<int>();
+            Random ran = new Random();
+            int Sum = 0;
+            for (int i=0;i<10;i++)
+            {
+                genericList.Add(ran.Next(3, 30));
+            }
+            int Max = genericList.Head.Data;
+            int Min = genericList.Head.Data;
+            Action<int> print = (i) => Console.WriteLine(i);
+            Action<int> max = (i) => 
+            {
+                if (i > Max) Max = i;
+                else return;
+            };
+            Action<int> min = (i) =>
+              {
+                  if (i < Min) Min = i;
+                  else return;
+              };
+            Action<int> sum = (i) =>
+             {
+                 Sum += i;
+             };
+            genericList.forEach(print);
+            genericList.forEach(max);
+            Console.WriteLine($"MAX={Max}");
+            genericList.forEach(min);
+            Console.WriteLine($"MIN={Min}");
+            genericList.forEach(sum);
+            Console.WriteLine($"SUM={Sum}");
+
+        }
+    }
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void forEach(Action<T> action)
+        {
+            Node<T> p = this.head;
+            for (; p != null; p = p.Next)
+            {
+                action(p.Data);
+            }
+        } 
+    }
+}
+
+
---
+++ W4Homework2/W4Homework2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace W4Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock1 = new Clock(2020,3,14,12,7,11);
+            clock1.alarm1.Alarm();
+        }
+    }
+
+    public delegate void clockHandler(object sender, TickEventArgs args);
+
+    public class TickEventArgs
+    {
+        public DateTime Time
+        {
+            get; set;
+        }
+    }
+
+    class alarm
+    {
+        public event clockHandler onAlarm;
+        public event clockHandler onTick;
+ 
+        public DateTime alarmTime
+        {
+            get; set;
+        }
+        public void Alarm()
+        {
+            TickEventArgs args = new TickEventArgs()
+            {
+                Time = DateTime.Now
+            };
+            TimeSpan span;
+            while (true)
+            {
+                onTick(this, args);
+                System.Threading.Thread.Sleep(1000);
+                span = alarmTime.Subtract(args.Time);
+                if (span.TotalSeconds<1&&span.TotalSeconds>0)
+                    onAlarm(this, args);
+            }
+        }
+    }
+
+    class Clock
+    { 
+        public alarm alarm1 = new alarm();
+
+        public Clock(int yyyy, int MM, int dd, int hh, int mm, int ss)
+        {
+            alarm1.alarmTime = new DateTime(yyyy, MM, dd, hh, mm, ss);
+            alarm1.onTick += addTime;
+            alarm1.onTick += show;
+            alarm1.onAlarm += ringing;
+        }
+
+        public void addTime(object sender,TickEventArgs args)
+        {
+            args.Time = DateTime.Now;
+        }
+
+        public void show(object sender, TickEventArgs args)
+        {
+            Console.WriteLine("Tick："+args.Time.ToString("T"));
+        }
+
+        public void ringing(object sender, TickEventArgs args)
+        {
+            Console.WriteLine("Ringing now!");
+        }
+    }
+}
+
+
