d411509aba7a672346a722a2a3f4756c2bf2efbe
Arthur-0 <1015982558@qq.com>
2020-04-05 23:56:38 +0800

    week7Homework

---
+++ W7Homework1/W7Homework1/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace W7Homework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtRecursionDepth = new System.Windows.Forms.TextBox();
+            this.txtleng = new System.Windows.Forms.TextBox();
+            this.txtper1 = new System.Windows.Forms.TextBox();
+            this.txtper2 = new System.Windows.Forms.TextBox();
+            this.txtTh1 = new System.Windows.Forms.TextBox();
+            this.txtTh2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmbpen = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.lblth2 = new System.Windows.Forms.Label();
+            this.lblth1 = new System.Windows.Forms.Label();
+            this.lblper2 = new System.Windows.Forms.Label();
+            this.lblper1 = new System.Windows.Forms.Label();
+            this.lblleng = new System.Windows.Forms.Label();
+            this.lbln = new System.Windows.Forms.Label();
+            this.panelForDraw = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRecursionDepth
+            // 
+            this.txtRecursionDepth.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtRecursionDepth.Location = new System.Drawing.Point(137, 93);
+            this.txtRecursionDepth.Name = "txtRecursionDepth";
+            this.txtRecursionDepth.Size = new System.Drawing.Size(161, 34);
+            this.txtRecursionDepth.TabIndex = 0;
+            // 
+            // txtleng
+            // 
+            this.txtleng.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtleng.Location = new System.Drawing.Point(137, 146);
+            this.txtleng.Name = "txtleng";
+            this.txtleng.Size = new System.Drawing.Size(161, 34);
+            this.txtleng.TabIndex = 1;
+            // 
+            // txtper1
+            // 
+            this.txtper1.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtper1.Location = new System.Drawing.Point(186, 201);
+            this.txtper1.Name = "txtper1";
+            this.txtper1.Size = new System.Drawing.Size(112, 34);
+            this.txtper1.TabIndex = 2;
+            // 
+            // txtper2
+            // 
+            this.txtper2.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtper2.Location = new System.Drawing.Point(186, 255);
+            this.txtper2.Name = "txtper2";
+            this.txtper2.Size = new System.Drawing.Size(112, 34);
+            this.txtper2.TabIndex = 3;
+            // 
+            // txtTh1
+            // 
+            this.txtTh1.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtTh1.Location = new System.Drawing.Point(162, 307);
+            this.txtTh1.Name = "txtTh1";
+            this.txtTh1.Size = new System.Drawing.Size(136, 34);
+            this.txtTh1.TabIndex = 4;
+            // 
+            // txtTh2
+            // 
+            this.txtTh2.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtTh2.Location = new System.Drawing.Point(162, 357);
+            this.txtTh2.Name = "txtTh2";
+            this.txtTh2.Size = new System.Drawing.Size(136, 34);
+            this.txtTh2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.cmbpen);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnDraw);
+            this.panel1.Controls.Add(this.lblth2);
+            this.panel1.Controls.Add(this.lblth1);
+            this.panel1.Controls.Add(this.lblper2);
+            this.panel1.Controls.Add(this.lblper1);
+            this.panel1.Controls.Add(this.lblleng);
+            this.panel1.Controls.Add(this.lbln);
+            this.panel1.Controls.Add(this.txtRecursionDepth);
+            this.panel1.Controls.Add(this.txtTh2);
+            this.panel1.Controls.Add(this.txtleng);
+            this.panel1.Controls.Add(this.txtTh1);
+            this.panel1.Controls.Add(this.txtper1);
+            this.panel1.Controls.Add(this.txtper2);
+            this.panel1.Font = new System.Drawing.Font("造字工房力黑（非商用）常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.panel1.Location = new System.Drawing.Point(613, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(322, 540);
+            this.panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 33);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Cayley Tree🌳生成";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("造字工房力黑（非商用）常规体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnClear.Location = new System.Drawing.Point(27, 466);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(104, 37);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmbpen
+            // 
+            this.cmbpen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbpen.Font = new System.Drawing.Font("造字工房悦圆演示版常规体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.cmbpen.FormattingEnabled = true;
+            this.cmbpen.Items.AddRange(new object[] {
+            "蓝色",
+            "紫色",
+            "粉色",
+            "绿色",
+            "橙色"});
+            this.cmbpen.Location = new System.Drawing.Point(162, 406);
+            this.cmbpen.Name = "cmbpen";
+            this.cmbpen.Size = new System.Drawing.Size(136, 37);
+            this.cmbpen.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 33);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "画笔颜色";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Font = new System.Drawing.Font("造字工房力黑（非商用）常规体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDraw.Location = new System.Drawing.Point(182, 466);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(116, 37);
+            this.btnDraw.TabIndex = 7;
+            this.btnDraw.Text = "DRAW";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.BtnDraw_Click);
+            // 
+            // lblth2
+            // 
+            this.lblth2.AutoSize = true;
+            this.lblth2.Location = new System.Drawing.Point(21, 359);
+            this.lblth2.Name = "lblth2";
+            this.lblth2.Size = new System.Drawing.Size(135, 33);
+            this.lblth2.TabIndex = 11;
+            this.lblth2.Text = "左分支角度";
+            // 
+            // lblth1
+            // 
+            this.lblth1.AutoSize = true;
+            this.lblth1.Location = new System.Drawing.Point(21, 309);
+            this.lblth1.Name = "lblth1";
+            this.lblth1.Size = new System.Drawing.Size(135, 33);
+            this.lblth1.TabIndex = 10;
+            this.lblth1.Text = "右分支角度";
+            // 
+            // lblper2
+            // 
+            this.lblper2.AutoSize = true;
+            this.lblper2.Location = new System.Drawing.Point(21, 255);
+            this.lblper2.Name = "lblper2";
+            this.lblper2.Size = new System.Drawing.Size(159, 33);
+            this.lblper2.TabIndex = 9;
+            this.lblper2.Text = "左分支长度比";
+            // 
+            // lblper1
+            // 
+            this.lblper1.AutoSize = true;
+            this.lblper1.Location = new System.Drawing.Point(21, 201);
+            this.lblper1.Name = "lblper1";
+            this.lblper1.Size = new System.Drawing.Size(159, 33);
+            this.lblper1.TabIndex = 8;
+            this.lblper1.Text = "右分支长度比";
+            // 
+            // lblleng
+            // 
+            this.lblleng.AutoSize = true;
+            this.lblleng.Font = new System.Drawing.Font("造字工房力黑（非商用）常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblleng.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblleng.Location = new System.Drawing.Point(20, 146);
+            this.lblleng.Name = "lblleng";
+            this.lblleng.Size = new System.Drawing.Size(111, 33);
+            this.lblleng.TabIndex = 7;
+            this.lblleng.Text = "主干长度";
+            // 
+            // lbln
+            // 
+            this.lbln.AutoSize = true;
+            this.lbln.Font = new System.Drawing.Font("造字工房力黑（非商用）常规体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbln.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbln.Location = new System.Drawing.Point(21, 93);
+            this.lbln.Name = "lbln";
+            this.lbln.Size = new System.Drawing.Size(111, 33);
+            this.lbln.TabIndex = 6;
+            this.lbln.Text = "递归深度";
+            // 
+            // panelForDraw
+            // 
+            this.panelForDraw.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panelForDraw.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelForDraw.Location = new System.Drawing.Point(23, 23);
+            this.panelForDraw.Name = "panelForDraw";
+            this.panelForDraw.Size = new System.Drawing.Size(584, 560);
+            this.panelForDraw.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 602);
+            this.Controls.Add(this.panelForDraw);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cayley Tree";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRecursionDepth;
+        private System.Windows.Forms.TextBox txtleng;
+        private System.Windows.Forms.TextBox txtper1;
+        private System.Windows.Forms.TextBox txtper2;
+        private System.Windows.Forms.TextBox txtTh1;
+        private System.Windows.Forms.TextBox txtTh2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbln;
+        private System.Windows.Forms.Label lblth2;
+        private System.Windows.Forms.Label lblth1;
+        private System.Windows.Forms.Label lblper2;
+        private System.Windows.Forms.Label lblper1;
+        private System.Windows.Forms.Label lblleng;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ComboBox cmbpen;
+        private System.Windows.Forms.Panel panelForDraw;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ W7Homework1/W7Homework1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W7Homework1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics graphics;
+        double th1;
+        double th2;
+        double per1;
+        double per2;
+        int n;
+        int leng;
+        Pen[] pens = { Pens.DodgerBlue, Pens.MediumPurple,Pens.LightPink,Pens.LimeGreen,Pens.OrangeRed };
+
+
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            th1 = double.Parse(txtTh1.Text) * Math.PI / 180;
+            th2 = double.Parse(txtTh2.Text) * Math.PI / 180;
+            per1 = double.Parse(txtper1.Text);
+            per2 = double.Parse(txtper2.Text);
+            leng = int.Parse(txtleng.Text);
+            n = int.Parse(txtRecursionDepth.Text);
+            Pen pen = pens[cmbpen.SelectedIndex];
+
+            if (graphics == null) graphics = panelForDraw.CreateGraphics();
+            drawCayleyTree(n, 200, 310, leng, -Math.PI / 2,pen);
+            
+        }
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th,Pen pen)
+        {
+            if (n == 0) return;
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1,pen);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1,pen);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2,pen);
+        }
+
+        void drawLine(double x0,double y0,double x1,double y1,Pen pen)
+        {
+            graphics.DrawLine(pen,(int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(panelForDraw.BackColor);
+          
+        }
+
+
+    }
+}
