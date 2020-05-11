a0e99c31733b3ba69cb36055c43fe402cb828187
Arthur-0 <1015982558@qq.com>
2020-05-11 22:11:50 +0800

    week9

---
+++ W9Homework1/SimpleCrawler/SimpleCrawler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleCrawler
+{
+    class SimpleCrawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        private static string startUrl = "http://www.cnblogs.com/dstang2000/";
+
+        static void Main(string[] args)
+        {
+            SimpleCrawler myCrawler = new SimpleCrawler();
+            
+            if (args.Length >= 1) startUrl = args[0];
+            myCrawler.urls.Add(startUrl, false);//加入初始页面
+            new Thread(myCrawler.Crawl).Start();
+        }
+
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了.... ");
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > 10) break;
+                Console.WriteLine("爬行" + current + "页面!");
+                string html = DownLoad(current); // 下载
+                urls[current] = true;
+                count++;
+                Parse(html);//解析,并加入新的链接
+                Console.WriteLine("爬行结束");
+            }
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                string rootURL = match.Value;
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                          .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                if (Regex.IsMatch(strRef, @"(html|htm)$"))
+                {
+                    strRef = absoluteUrl(strRef, rootURL);
+
+                    if (urls[strRef] == null) urls[strRef] = false;
+ 
+                }
+
+            }
+        }
+
+        static private string absoluteUrl(string url,string rootUrl)
+        {
+            if(Regex.IsMatch(url,@"^[(http|https)://]"))
+            {
+                return url;
+            }
+            if(Regex.IsMatch(url,@"^/"))
+            {
+                return rootUrl + url;
+            }
+            if(Regex.IsMatch(url,@"^//"))
+            {
+                return "http:" + url;
+            }
+            if(Regex.IsMatch(url,@"^../"))
+            {
+                url = url.Substring(3);
+                int endIndex = rootUrl.LastIndexOf("/");
+                return absoluteUrl(url, rootUrl.Substring(0, endIndex));
+            }
+            if (Regex.IsMatch(url,@"^./"))
+            {
+                return absoluteUrl(url.Substring(2), rootUrl);
+            }
+
+            int end = rootUrl.LastIndexOf("/");
+            return rootUrl.Substring(0, end) + "/" + url;
+        }
+        
+    }
+    
+}
+
