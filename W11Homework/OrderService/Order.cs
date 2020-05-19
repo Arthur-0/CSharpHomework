a58ace7052c208aed65797b29b9efeab5c79be58
Arthur-0 <1015982558@qq.com>
2020-05-19 13:54:01 +0800

    week11

---
+++ W11Homework/OrderService/Order.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OrderApp
+{
+
+    /**
+     **/
+    public class Order : IComparable<Order>
+    {
+
+        private List<OrderItem> items;
+
+        [Key]
+        public string OrderId { get; set; }
+        public String CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        public string CustomerName { get => (Customer != null) ? Customer.Name : ""; }
+
+        public DateTime CreateTime { get; set; }
+
+        public Order()
+        {
+            OrderId = Guid.NewGuid().ToString();
+            items = new List<OrderItem>();
+            CreateTime = DateTime.Now;
+        }
+
+
+        public Order(string orderId, Customer customer, List<OrderItem> items)
+        {
+            this.OrderId = orderId;
+            this.Customer = customer;
+            this.CreateTime = DateTime.Now;
+            this.items = (items == null) ? new List<OrderItem>() : items;
+        }
+
+        public List<OrderItem> Items
+        {
+            get { return items; }
+        }
+
+        public double TotalPrice
+        {
+            get => items.Sum(item => item.TotalPrice);
+        }
+
+        public void AddItem(OrderItem orderItem)
+        {
+            if (Items.Contains(orderItem))
+                throw new ApplicationException($"添加错误：订单项{orderItem.GoodsName} 已经存在!");
+            Items.Add(orderItem);
+            using (var context = new OrderContext())
+            {
+                context.Entry(orderItem).State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void RemoveItem(OrderItem orderItem)
+        {
+            Items.Remove(orderItem);
+            using (var context = new OrderContext())
+            {
+                context.orderItems.Remove(orderItem);
+                context.SaveChanges();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append($"Id:{OrderId}, customer:{Customer},orderTime:{CreateTime},totalPrice：{TotalPrice}");
+            items.ForEach(od => strBuilder.Append("\n\t" + od));
+            return strBuilder.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null &&
+                   OrderId == order.OrderId;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -531220479;
+            hashCode = hashCode * -1521134295 + OrderId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(CustomerName);
+            hashCode = hashCode * -1521134295 + CreateTime.GetHashCode();
+            return hashCode;
+        }
+
+        public int CompareTo(Order other)
+        {
+            if (other == null) return 1;
+            return this.OrderId.CompareTo(other.OrderId);
+        }
+    }
+}
---
+++ W11Homework/OrderService/OrderService.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace OrderApp
+{
+
+    /**
+     * The service class to manage orders
+     * */
+    public class OrderService
+    {
+
+        private List<Order> orders;
+
+        public List<Order> Orders { get => orders; set => orders = value; }
+        public OrderService()
+        {
+          
+            Orders = new List<Order>();
+            using (var context=new OrderContext())
+            {
+                foreach(Order order in context.orders)
+                {
+                    this.Orders.Add(order);
+                }
+            }
+        }
+
+        public Order GetOrder(string id)
+        {
+            var query = Orders
+                    .Where(order => order.OrderId == id)
+                    .OrderBy(o => o.TotalPrice);
+            return query as Order;
+        }
+
+        public Order AddOrder(Order order1)
+        {
+            this.Orders.Add(order1);
+            try
+            {
+                using (var context = new OrderContext())
+                {
+                    
+                    context.orders.Add(order1);
+                    context.SaveChanges();
+                }
+                return order1;
+            }
+            catch (Exception e)
+            {
+                //TODO 需要更加错误类型返回不同错误信息
+                throw new ApplicationException($"添加错误: {e.Message}");
+            }
+        }
+
+        //删除订单
+        public void DeleteOrder(string ID)
+        {
+            Order order1 = SearchByID(ID);
+            if(order1!=null)
+            {
+                Orders.Remove(order1);
+                using (var context = new OrderContext())
+                {
+                    context.orders.Remove(order1);
+                    context.SaveChanges();
+                    Console.WriteLine("成功删除订单");
+                }
+            }
+            
+        }
+
+        public Order SearchByID(string ID)
+        {
+            var query = Orders
+                    .Where(order => order.OrderId==ID)
+                    .OrderBy(o => o.TotalPrice);
+            return query as Order;
+        }
+
+        public List<Order> QueryOrdersByGoodsName(string goodsName)
+        {
+            var query = Orders
+                    .Where(order => order.Items.Exists(item => item.GoodsName == goodsName))
+                    .OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+        public List<Order> QueryOrdersByCustomerName(string customerName)
+        {
+            return Orders
+                .Where(order => order.CustomerName == customerName)
+                .OrderBy(o => o.TotalPrice)
+                .ToList();
+        }
+
+        public void UpdateOrder(Order newOrder)
+        {
+            Order oldOrder = GetOrder(newOrder.OrderId);
+            if (oldOrder == null)
+                throw new ApplicationException($"修改错误：订单 {newOrder.OrderId} 不存在!");
+
+            using (var context = new OrderContext())
+            {
+                context.Entry(newOrder).State = EntityState.Modified;
+                context.orderItems.AddRange(newOrder.Items);
+                context.SaveChanges();
+
+            }
+        }
+
+
+
+        //删除订单明细
+        public void DeleteOrderItem(OrderItem orderItem)
+        {
+            using (var context = new OrderContext())
+            {
+                context.orderItems.Remove(orderItem);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Sort()
+        {
+            Orders.Sort();
+        }
+
+        public void Sort(Func<Order, Order, int> func)
+        {
+            Orders.Sort((o1, o2) => func(o1, o2));
+        }
+
+        public void Export(String fileName)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(fileName, FileMode.Create))
+            {
+                xs.Serialize(fs, Orders);
+            }
+        }
+
+        public void Import(string path)
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                List<Order> temp = (List<Order>)xs.Deserialize(fs);
+                temp.ForEach((Action<Order>)(order =>
+                {
+                    if (!this.Orders.Contains(order))
+                    {
+                        this.Orders.Add(order);
+                    }
+                }));
+            }
+        }
+
+        public object QueryByTotalAmount(float amout)
+        {
+            return Orders
+               .Where(order => order.TotalPrice >= amout)
+               .OrderByDescending(o => o.TotalPrice)
+               .ToList();
+        }
+
+        public static void AddGood(Goods goods)
+        {
+            try
+            {
+                using (var context = new OrderContext())
+                {
+                    context.GoodItems.Add(goods);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO 需要更加错误类型返回不同错误信息
+                throw new ApplicationException($"添加错误!");
+            }
+        }
+
+        public static List<Goods> GetAll()
+        {
+            using (var context = new OrderContext())
+            {
+                return context.GoodItems.ToList();
+            }
+        }
+
+        public static void AddCustomer(Customer customer)
+        {
+            try
+            {
+                using (var db = new OrderContext())
+                {
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                //TODO 需要更加错误类型返回不同错误信息
+                throw new ApplicationException($"添加错误!");
+            }
+        }
+    }
+}
